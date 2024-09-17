52891c9a6166357bc0a3e5651b4e4737b9161b47
Bilal-pasha <billopasho56@gmail.com>
2024-09-17 23:54:38 +0500

    toast added when user sign in successfully

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -3,13 +3,13 @@
 import { useSession } from 'next-auth/react';
 import SignIn from '@/components/SignIn/SignIn';
 import { useRouter } from 'next/navigation';
-
+import toast from 'react-hot-toast';
 const AuthLayout: React.FC = () => {
   const { data: session } = useSession();
   const route = useRouter()
   return (
     <>
-      {session ? route.push("/Home") : <SignIn />}
+      {session ? toast.success("Sign In Successfully") && route.push("/Home") : <SignIn />}
     </>
   );
 };
